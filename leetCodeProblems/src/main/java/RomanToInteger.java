29c52b99d71b459cd155755649ada36ca7bf6a41
predaalex <preda.alex30@yahoo.com>
2022-08-26 12:14:51 +0300

    13. Roman to Integer

---
+++ leetCodeProblems/src/main/java/RomanToInteger.java
@@ -0,0 +1,93 @@
+import java.util.HashMap;
+
+/* 13.RomanToInteger
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ */
+
+public class RomanToInteger {
+
+    static int romanToInt(String s) {
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        hashMap.put("I", 1);
+        hashMap.put("V", 5);
+        hashMap.put("X", 10);
+        hashMap.put("L", 50);
+        hashMap.put("C", 100);
+        hashMap.put("D", 500);
+        hashMap.put("M", 1000);
+
+        int finalInt = 0;
+
+        int aux = 0;
+
+
+        for(int i = s.length() - 1; i >= 0; i--){
+
+            String letter = String.valueOf(s.charAt(i));
+
+            if(hashMap.containsKey(letter)){
+
+                int value = hashMap.get(letter);
+
+                if(value >= aux) {
+
+                        finalInt += hashMap.get(letter);
+
+                } else if (aux / value <= 10) { // 1000 / 1 = 10000 | 1000 / 100 = 10
+
+                    finalInt -= value;
+
+                } else {
+
+                    System.out.println("Invalid number");
+
+                    return -1;
+                }
+                aux = value;
+            }
+        }
+
+        return finalInt;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "MCMXCIV";
+
+        System.out.println(romanToInt(s));
+    }
+}
