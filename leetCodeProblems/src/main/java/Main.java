7215116a64896cfdbb3fd98eb9ac785beb3aa936
predaalex <preda.alex30@yahoo.com>
2022-08-26 14:57:21 +0300

    234. Palindrom Linked List

--- leetCodeProblems/src/main/java/Main.java
+++ leetCodeProblems/src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("salut");
+
+        // 234. Palindrom Linked List
+
+        boolean palindrom = false;
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(3);
+        linkedList.add(2);
+        linkedList.add(1);
+
+        System.out.println(linkedList);
+
+        while(linkedList.size() > 1) {
+            if(Objects.equals(linkedList.getFirst(), linkedList.getLast())){
+                linkedList.removeLast();
+                linkedList.removeFirst();
+            } else {
+                break;
+            }
+        }
+
+        if(linkedList.size() <= 1)
+            palindrom = true;
+
+        System.out.println(palindrom);
     }
 }
