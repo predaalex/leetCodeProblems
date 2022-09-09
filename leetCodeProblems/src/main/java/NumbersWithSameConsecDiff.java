5f4c12650a91e2ea8cb089f7dd9ea2ce72a9d511
predaalex <preda.alex30@yahoo.com>
2022-09-09 15:42:16 +0300

    967. Numbers With Same Consecutive Differences v2

--- leetCodeProblems/src/main/java/NumbersWithSameConsecDiff.java
+++ leetCodeProblems/src/main/java/NumbersWithSameConsecDiff.java
@@ -14,55 +14,52 @@
         Input: n = 2, k = 1
         Output: [10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]*/
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-public class NumbersWithSameConsecDiff {
+        public class NumbersWithSameConsecDiff {
     public static void main(String[] args) {
 
-        int n = 7;
-        int k = 9;
+        int n = 2;
+        int k = 0;
         System.out.println(Arrays.toString(numsSameConsecDiff(n, k)));
-
     }
 
     private static int[] numsSameConsecDiff(int n, int k) {
+        Set<Integer> set = new HashSet<>();
 
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        nrCreate(set, n, k, 0, 0);
 
-        for(int i = (int) Math.pow(10, n - 1); i < Math.pow(10, n); i++)
-            if(nrValidation(i, k))
-                arrayList.add(i);
-
-        int[] arr = new int[arrayList.size()];
+        int[] arr = new int[set.size()];
 
         int j = 0;
-
-        for(Integer i : arrayList)
+        for(Integer i : set)
             arr[j++] = i;
 
         return arr;
     }
 
-    private static boolean nrValidation(int n, int k) {
-
-        //181
-        boolean valid = false;
-        int i = n;
-        int a;
-
-        while(i >= 10){
-
-            a = i % 10;
-            i = i / 10;
-
-            if( Math.abs(a - (i % 10)) == k || Math.abs((i % 10) - a) == k){
-                valid = true;
-            } else {
-                valid = false;
-                break;
-            }
+    private static void nrCreate(Set<Integer> set, int n, int k, int len, int num) {
+        if(len == n){
+            set.add(num);
+            return;
         }
-        return valid;
+
+        if(num == 0){
+            for(int i = 1; i < 10; i++) {
+                nrCreate(set, n, k , len + 1, i);
+            }
+        } else {
+            int lastDigit = num % 10;
+            int after = lastDigit + k;
+            int before = lastDigit - k;
+            if(after >= 0 && after <= 9)
+                nrCreate(set, n, k, len + 1, num * 10 + after);
+            if(before >= 0 && before <= 9)
+                nrCreate(set, n, k, len + 1, num * 10 + before);
+        }
     }
+
+
 }
