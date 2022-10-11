f8253c3f7b9f5c2929e355603925fde57afa0e07
predaalex <preda.alex30@yahoo.com>
2022-10-11 13:05:18 +0300

    334. Increasing Triplet Subsequence v2

--- leetCodeProblems/src/main/java/IncresingTripletSubsequence.java
+++ leetCodeProblems/src/main/java/IncresingTripletSubsequence.java
@@ -1,3 +1,24 @@
+//        334. Increasing Triplet Subsequence
+//
+//        Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+//=======================================================================================
+//        Example 1:
+//
+//        Input: nums = [1,2,3,4,5]
+//        Output: true
+//        Explanation: Any triplet where i < j < k is valid.
+//----------------------------------------------------------------------------------
+//        Example 2:
+//
+//        Input: nums = [5,4,3,2,1]
+//        Output: false
+//        Explanation: No triplet exists.
+//---------------------------------------------------------------------------------
+//        Example 3:
+//
+//        Input: nums = [2,1,5,0,4,6]
+//        Output: true
+//        Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
 public class IncresingTripletSubsequence {
     public static void main(String[] args) {
         int[] nums = {1,5,0,4,1,3};
@@ -6,18 +27,18 @@ public class IncresingTripletSubsequence {
 
     private static boolean increasingTriplet(int[] nums) {
 
-        int start, middle, end;
+        int smallest = Integer.MAX_VALUE;
+        int biggest = Integer.MAX_VALUE;
 
-        for (start = 0; start < nums.length - 2; start++) {
-            for (middle = start + 1; middle < nums.length - 1; middle++) {
-                if (nums[start] < nums[middle]) {
-                    for (end = nums.length - 1; end > middle; end--) {
-                        if (nums[middle] < nums[end])
-                            return true;
-                    }
-                }
-            }
+        for(int n : nums){
+            if(n <= smallest){
+                smallest = n;
+            } else if( n <= biggest) {
+                biggest = n;
+            } else
+                return true;
         }
+
         return false;
     }
 }
