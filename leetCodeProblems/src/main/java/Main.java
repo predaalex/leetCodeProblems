a42907933f83b648441c4ac2bbc113c44d29e894
predaalex <preda.alex30@yahoo.com>
2022-10-25 11:12:56 +0300

    main problem

--- leetCodeProblems/src/main/java/Main.java
+++ leetCodeProblems/src/main/java/Main.java
@@ -1,26 +1,62 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {-1,-1,0,1,1,0};
-        int leftSum = 0, rightSum = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-            int startLeft = i - 1;
-            leftSum = 0;
-            while(startLeft >= 0) {
-                leftSum+=nums[startLeft];
-                startLeft--;
-            }
-
-            int startRight = i + 1;
-            rightSum = 0;
-            while(startRight <= nums.length - 1) {
-                rightSum+=nums[startRight];
-                startRight++;
-            }
-
-            if(rightSum == leftSum)
-                System.out.println(i);
-        }
-        System.out.println(-1);
+        String[] temp = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"};
+        List<String> arr = new ArrayList<>(Arrays.asList(temp));
+        System.out.println(findPermutation(arr));
     }
+
+    private static int findMax(List<String> arr) {
+
+        TreeSet<Character> uniqueChars = new TreeSet<>();
+
+        for (String word : arr) {
+            TreeSet<Character> uniqueWord = new TreeSet<>();
+            boolean contains = false;
+            for (char c : word.toCharArray()) {
+
+                if (uniqueWord.contains(c) || uniqueChars.contains(c)) {
+                    contains = true;
+                    break;
+                } else
+                    uniqueWord.add(c);
+            }
+
+            if (!contains)
+                for (char c : word.toCharArray())
+                    uniqueChars.add(c);
+            else {
+                System.out.println(uniqueChars + " " + arr + " max " + uniqueChars.size());
+                return uniqueChars.size();
+            }
+
+
+        }
+        return uniqueChars.size();
+    }
+
+    private static int findPermutation(List<String> arr) {
+        if (arr == null || arr.size() == 0)
+            return 0;
+        int max = 0;
+        return permutations(arr, 0, max);
+    }
+
+    private static int permutations(List<String> arr, int currentIndex, int max) {
+
+        if (currentIndex == arr.size() - 1) {
+
+            return Math.max(max, findMax(arr));
+        }
+
+        for (int i = currentIndex; i < arr.size(); i++) {
+            Collections.swap(arr, currentIndex, i);
+            max = Math.max(max, permutations(arr, currentIndex + 1, max));
+            Collections.swap(arr, currentIndex, i);
+        }
+
+        return max;
+    }
+
 }
