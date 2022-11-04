e172231cf0fa2a3ba4961af09a6766f42050efa1
predaalex <preda.alex30@yahoo.com>
2022-11-04 17:04:53 +0200

    problems

---
+++ leetCodeProblems/src/main/java/LongestPalindromeByConcatenatingTwoLetterWords.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class LongestPalindromeByConcatenatingTwoLetterWords {
+    public static void main(String[] args) {
+        String[] words = {"bb", "bb"};
+
+        System.out.println(longestPalindrome(words));
+    }
+
+    private static int longestPalindrome(String[] words) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        int len = 0;
+
+        for(String word : words){
+
+            String wordPair = String.valueOf(word.charAt(1)) +
+                    word.charAt(0);
+            if(hashMap.containsKey(wordPair) && hashMap.get(wordPair) > 0){
+                len += 4;
+                hashMap.put(wordPair, hashMap.get(wordPair) - 1);
+            } else {
+                hashMap.put(word, hashMap.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        for(String word : hashMap.keySet()){
+            if(word.charAt(0) == word.charAt(1) && hashMap.get(word) % 2 == 1){
+                len += 2;
+                break;
+            }
+        }
+
+        return len;
+    }
+}
---
+++ leetCodeProblems/src/main/java/LongestRepeatingCharReplacement.java
@@ -0,0 +1,32 @@
+public class LongestRepeatingCharReplacement {
+    public static void main(String[] args) {
+        String s = "ABBB";
+        int k = 2;
+
+        System.out.println(characterReplacement(s, k));
+    }
+
+    private static int characterReplacement(String s, int k) {
+        int longest = 0;
+        for(int i = 0; i < s.length() - 1; i++) {
+            char c = s.charAt(i);
+            int aux = k;
+            int counter = 0;
+            while(i + counter < s.length()){
+                if(s.charAt(i + counter) != c)
+                    aux--;
+                if(aux < 0)
+                    break;
+                counter++;
+            }
+
+            if (counter == s.length())
+                return counter;
+
+            if(longest < counter)
+                longest = counter;
+        }
+
+        return longest;
+    }
+}
