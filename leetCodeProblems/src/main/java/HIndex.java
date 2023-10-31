795812eeb40ffd2d12235c5f201718f485585ab1
predaalex <preda.alex30@yahoo.com>
2023-10-31 10:33:19 +0200

    274. H-Index

---
+++ leetCodeProblems/src/main/java/HIndex.java
@@ -0,0 +1,45 @@
+package main.java;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+//274. H-Index
+//Medium
+//
+//Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+//
+//According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+//
+//---------------------------------------------------------------
+//Example 1:
+//
+//Input: citations = [3,0,6,1,5]
+//Output: 3
+//Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+//Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+//
+//---------------------------------------------------------------
+//Example 2:
+//
+//Input: citations = [1,3,1]
+//Output: 1
+public class HIndex {
+    public static void main(String[] args) {
+        int[] citations1 = {3, 0, 6, 1, 5};
+        int[] citations2 = {1, 3, 1};
+
+        System.out.println(hIndex(citations1));
+    }
+
+    private static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+
+        for(int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] <= Math.abs(i - citations.length + 1))
+                return Math.abs(i - citations.length + 1);
+
+        }
+
+        return citations.length;
+    }
+}
