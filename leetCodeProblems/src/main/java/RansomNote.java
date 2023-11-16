e9e9da187d3631d8524f04d0f04da1c9eb8cf4dd
predaalex <preda.alex30@yahoo.com>
2023-11-16 16:55:59 +0200

    383. Ransom Note

--- leetCodeProblems/src/main/java/RansomNote.java
+++ leetCodeProblems/src/main/java/RansomNote.java
@@ -1,28 +1,39 @@
 package main.java;
 
-//        Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+import java.util.HashMap;
+//383. Ransom Note
+//Easy
+//Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 //
-//        Each letter in magazine can only be used once in ransomNote.
+//Each letter in magazine can only be used once in ransomNote.
 //
-//        Example 1:
+//Example 1:
 //
-//        Input: ransomNote = "a", magazine = "b"
-//        Output: false
-//        Example 2:
+//Input: ransomNote = "a", magazine = "b"
+//Output: false
+//Example 2:
 //
-//        Input: ransomNote = "aa", magazine = "ab"
-//        Output: false
-//        Example 3:
+//Input: ransomNote = "aa", magazine = "ab"
+//Output: false
+//Example 3:
 //
-//        Input: ransomNote = "aa", magazine = "aab"
-//        Output: true
+//Input: ransomNote = "aa", magazine = "aab"
+//Output: true
 public class RansomNote {
     static boolean canConstruct(String ransomNote, String magazine) {
-        for(int i = 0; i < magazine.length(); i++) {
-            ransomNote = ransomNote.replaceFirst( String.valueOf(magazine.charAt(i)), "");
-        }
+        HashMap<Character, Integer> hashMap1 = new HashMap<>();
+        HashMap<Character, Integer> hashMap2 = new HashMap<>();
 
-        return ransomNote.length() == 0;
+        for(char c : ransomNote.toCharArray())
+            hashMap1.put(c, hashMap1.getOrDefault(c, 0) + 1);
+        for(char c : magazine.toCharArray())
+            hashMap2.put(c, hashMap2.getOrDefault(c, 0) + 1);
+
+        for(Character c : hashMap1.keySet()){
+            if(!hashMap2.containsKey(c) || hashMap2.get(c) < hashMap1.get(c))
+                return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
